01ff8f5e8a33395852546996d6a8a1bd89ae22c2
suvignan <srisuvignan@gmail.com>
2026-05-26 22:15:40 +0530

    re

--- koru-frontend/vite.config.js
+++ koru-frontend/vite.config.js
@@ -7,12 +7,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
-    },
   },
   server: {
     host: true,
